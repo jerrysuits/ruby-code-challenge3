9eee14c339c3e950de46c0f1200b1db5791e345a
Gerald <adudagerald@gmail.com>
2023-05-28 23:59:55 +0300

    done with author class

--- articles.rb
+++ articles.rb
@@ -17,6 +17,14 @@ class Author
      def magazines
      articles.map { |article| article.magazine }.uniq
      end
+
+     def add_article(magazine, title) #creates a new article instance and associates it with author and magazine
+          Article.new(self, magazine, title)
+     end
+
+     def topic_areas #returns unique arr of strings of magazines author has contributed to
+          magazines.map { |magazine| magazine.category }.uniq
+     end
 end
 
 #second class
