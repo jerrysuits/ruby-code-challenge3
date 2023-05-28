9b62f65779a940aaf452866567fd5199a08adfb3
Gerald <adudagerald@gmail.com>
2023-05-28 20:49:38 +0300

    done with magazine class

--- articles.rb
+++ articles.rb
@@ -13,6 +13,27 @@ end
 
 #second class
 class Magazine
+     attr_accessor :name :category  #creates setter and getter methods for name and category
+     @@all = [] # initializes emptu array to put magazines instances
+
+     def initialize (name, category) #initializes name and category
+          @name=name
+          @category=category
+          @@all << self # pushes y=the instances to the array
+     end
+
+     def name #this method returns the name of the magazine
+          @name
+     end
+
+     def category #this method returns the category of the magazine
+          @category
+     end
+
+     def self.all #returns an array contINNING MAGAZINE INSTANCES
+          @@all
+     end
+
 end
 
 #third class
