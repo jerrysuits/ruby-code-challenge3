219e641a07fc4fdfd6d16280b6db924bcea36073
Gerald <adudagerald@gmail.com>
2023-05-28 20:57:27 +0300

    done with article class

--- articles.rb
+++ articles.rb
@@ -38,4 +38,21 @@ end
 
 #third class
 class Article
+     attr_reader :name :magazine :title
+     @@all = []
+
+     def initialize (author, magazine, title)
+          @author=author
+          @magazine=magazine
+          @title=title
+          @@all << self
+     end
+
+     def title
+          @title
+     end
+
+     def self.all
+          @@all
+     end
 end
