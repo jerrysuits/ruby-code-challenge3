b2abe542e4fc1646a1abb6c36e0a76b6c82a62ff
Gerald <adudagerald@gmail.com>
2023-05-28 20:06:06 +0300

    finished creating author class

--- articles.rb
+++ articles.rb
@@ -1,5 +1,14 @@
 #first class
 class Author
+     attr_reader :name #creates getter method
+
+     def initialize (name) #this method initiqalizes an authors name
+          @name=name
+     end
+
+     def name #this method returns the name of the author
+          @name
+     end
 end
 
 #second class
